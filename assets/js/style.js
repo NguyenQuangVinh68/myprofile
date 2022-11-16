2062d80cc30828f5bd0e1e06a98520f9bb63aed9
Quang Vinh <giusevinh12@gmail.com>
2022-11-16 18:07:50 +0700

    update

--- assets/js/style.js
+++ assets/js/style.js
@@ -2,3 +2,9 @@ window.addEventListener("scroll", () => {
   var header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+function changeIcon(item) {
+  var icon = item.querySelector("#icon");
+  icon.classList.toggle("fa-xmark");
+  icon.classList.toggle("fa-bars-staggered");
+}
