2544f397cd32fe76f637a61d7df1768cc1196879
Quang Vinh <giusevinh12@gmail.com>
2023-05-13 21:38:49 +0700

    change link

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,10 +1,10 @@
 window.addEventListener("scroll", () => {
-  var header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 0);
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 function changeIcon(item) {
-  var icon = item.querySelector("#icon");
-  icon.classList.toggle("fa-xmark");
-  icon.classList.toggle("fa-bars-staggered");
+    var icon = item.querySelector("#icon");
+    icon.classList.toggle("fa-xmark");
+    icon.classList.toggle("fa-bars-staggered");
 }
